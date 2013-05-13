24c6fcad9f9358e51abb1f8886feb82c4b9f4d57
Mateoq <quinterom1592@gmail.com>
2013-05-13 17:29:59 -0500

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RibbitApp::Application.config.secret_token = '3f6e4f8ec7e2837a1bddff58260cadcfe7acbd9ed7e174d6de1443d415cd23c4eb4851b8def9439912b8eb968e60e81cabf861bc6adc839800d74cc49e866bc3'
